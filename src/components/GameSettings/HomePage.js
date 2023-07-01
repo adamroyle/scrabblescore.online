685e2364a2596e896d8261f5d4d57f2fc7c0a5ff
Adam Royle <adam@ifunk.net>
2023-07-01 18:57:51 +1000

    Remove instructional text

--- src/components/GameSettings/HomePage.js
+++ src/components/GameSettings/HomePage.js
@@ -1,44 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const HomePage = (props) => {
-  return ( 
+  return (
     <div className="container">
       <div className="row">
         <div className="col-sm-12">
-          <img id="big-logo" src="logo.png" alt="Scrabble score logo"/>
-          <p>
-             This Scra Score Sheet keeps track of the game, and replaces traditional pen-and-paper score sheets for the Scrabble crossword board game.
-             Enjoy the game with your friends and family while this Scrabble Score Keeper does the math for you.
-          </p>
-          <p>
-             Simply fill in the players’ names in order that they will
-             take turns and press the START button.
-          </p>
+          <img id="big-logo" src="logo.png" alt="Scrabble score logo" />
         </div>
       </div>
-      <div className="homepage-children">
-      { props.children }
-      </div>
-      <div className="row">
-        <div className="col-sm-12">
-          <h5>Features</h5>
-          <ul>
-            <li>Supports official Scrabble scoring rules, including leftovers accounting</li>
-            <li>Shows detailed game progress</li>
-            <li>Supports unlimited undo</li>
-            <li>Restores score sheets when relaunching the app</li>
-          </ul>
-          
-          <h5>Limitations</h5>
-          <ul>
-            <li>Does not validate words against the Scrabble Dictionary</li>
-            <li>Only supports the english language</li>
-            <li>Does not archive score sheets</li>
-          </ul>
-        </div>
-      </div>
+      <div className="homepage-children">{props.children}</div>
     </div>
   );
-}
+};
 
 export default HomePage;
--- src/components/ScrabbleScoreKeeper/ScoreKeeper.js
+++ src/components/ScrabbleScoreKeeper/ScoreKeeper.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Game from '../../logic/game';
-import ScoreGrid from '../ScoreGrid/ScoreGrid';
-import ScoreGridMobile from '../ScoreGrid/ScoreGridMobile';
-import CallPlayerToAction from './CallPlayerToAction';
-import InGameControls from './InGameControls';
-import InGameOverControls from './InGameOverControls';
-import { getPersistedState, persistState } from '../../logic/util';
+import React from "react";
+import Game from "../../logic/game";
+import ScoreGrid from "../ScoreGrid/ScoreGrid";
+import ScoreGridMobile from "../ScoreGrid/ScoreGridMobile";
+import CallPlayerToAction from "./CallPlayerToAction";
+import InGameControls from "./InGameControls";
+import InGameOverControls from "./InGameOverControls";
+import { getPersistedState, persistState } from "../../logic/util";
 
 class ScoreKeeper extends React.Component {
   constructor(props) {
@@ -30,9 +30,9 @@ class ScoreKeeper extends React.Component {
   handleSetGame(currentGame) {
     const { game, games } = this.state;
     const { playerNames } = this.props;
-    const newState = {game: currentGame, games: [...games.slice(), game]};
+    const newState = { game: currentGame, games: [...games.slice(), game] };
     this.setState(newState);
-    persistState("gameState", {'playerNames': playerNames, ...newState});
+    persistState("gameState", { playerNames: playerNames, ...newState });
   }
 
   handleUndo() {
@@ -43,7 +43,7 @@ class ScoreKeeper extends React.Component {
     const game = games[games.length - (ignoreLastGame ? 2 : 1)];
     const newState = { game, games: previousGames };
     this.setState(newState);
-    persistState("gameState", {'playerNames': playerNames, ...newState});
+    persistState("gameState", { playerNames: playerNames, ...newState });
   }
 
   /**
@@ -56,13 +56,11 @@ class ScoreKeeper extends React.Component {
   undoShouldIgnoreLastGame() {
     const { game, games } = this.state;
     const lastGame = games[games.length - 1];
-    if (!lastGame)
-      return false;
+    if (!lastGame) return false;
     const playerIndex = lastGame.getCurrentPlayerIndex();
     const lastPlayerTurn = lastGame.playersTurns[playerIndex].slice(-1)[0];
     const currentPlayerTurn = game.playersTurns[playerIndex].slice(-1)[0];
-    if (!lastPlayerTurn.words.length)
-      return false;
+    if (!lastPlayerTurn.words.length) return false;
     return JSON.stringify(lastPlayerTurn) === JSON.stringify(currentPlayerTurn);
   }
 
@@ -73,9 +71,13 @@ class ScoreKeeper extends React.Component {
     const winners = game.getWinners();
     if (winners.length > 1) {
       const winnersTie = game.getWinners(turnBeforeLeftOvers);
-      return winnersTie.map(winnerIndex => (winnersTie.length > 1
-        ? `${playerNames[winnerIndex]}: ${game.getTotalScore(winnerIndex, turnBeforeLeftOvers)} points`
-        : `${playerNames[winnerIndex]} won with ${game.getTotalScore(winnerIndex, turnBeforeLeftOvers)} points!`)).join(', ');
+      return winnersTie
+        .map((winnerIndex) =>
+          winnersTie.length > 1
+            ? `${playerNames[winnerIndex]}: ${game.getTotalScore(winnerIndex, turnBeforeLeftOvers)} points`
+            : `${playerNames[winnerIndex]} won with ${game.getTotalScore(winnerIndex, turnBeforeLeftOvers)} points!`
+        )
+        .join(", ");
     }
     return `${playerNames[winners[0]]} won with ${game.getTotalScore(winners[0])} points!`;
   }
@@ -96,7 +98,7 @@ class ScoreKeeper extends React.Component {
 
     const toDisplayCallPlayerToAction = () => {
       if (!isMobile) {
-        return  <CallPlayerToAction game={game} playerNames={playerNames} isMobile={isMobile}/>
+        return <CallPlayerToAction game={game} playerNames={playerNames} isMobile={isMobile} />;
       }
     };
 
@@ -104,52 +106,22 @@ class ScoreKeeper extends React.Component {
       <div className="score-keeper">
         <div className="container">
           <div id="small-logo" onClick={onNewGame}>
-             <img src="logo.png" alt="Scrabble score logo"/>
+            <img src="logo.png" alt="Scrabble score logo" />
           </div>
           <h1 className="title">Score Sheet</h1>
-          {isMobile
-            ? <ScoreGridMobile playerNames={playerNames} game={game} language={language} />
-            : <ScoreGrid playerNames={playerNames} game={game} language={language} />
-          }
-          {!game.areLeftOversSubmitted()
-            ? toDisplayCallPlayerToAction()
-            : (
-              <div className="winner">
-                <h1>{this.renderWinner()}</h1>
-              </div>
-            )
-              }
-          {!game.isGameOver()
-            ? <InGameControls {...controlProps} />
-            : <InGameOverControls {...controlProps} />
-              }
-          <h3 className="instructions">Instructions</h3>
-        <ul>
-          <li>To submit a word, type the word in the input box (the white rectangle above the buttons) then press END TURN.</li>
-          <li>When scoring a word with a tile on a prime square (e.g., double-word), press on the coresponding tile in the input box
-              then choose a corresponding option from the window.</li>
-          <li>If you made a mistake, you may use unlimited UNDO.</li>
-
-          <li>
-            When scoring multiple words, use ADD WORD.
-          </li>
-          <li>
-            When a player uses all seven tiles in a single turn, press BINGO.
-            This adds a 50 points bonus.
-          </li>
-          <li>
-            When using a BLANK tile, press on the corresponding tile and mark it as such.
-          </li>
-          <li>
-            When all the players completed their last turn, press END GAME.
-            Players are then asked to enter their leftover tiles.
-            Players with leftover tiles get their leftover points deducted from their score.
-            Players with no leftovers tiles collect the leftover points of other players.
-          </li>
-          <li>
-            To start a new game, press on the logo at the top of the page.
-          </li>
-        </ul>
+          {isMobile ? (
+            <ScoreGridMobile playerNames={playerNames} game={game} language={language} />
+          ) : (
+            <ScoreGrid playerNames={playerNames} game={game} language={language} />
+          )}
+          {!game.areLeftOversSubmitted() ? (
+            toDisplayCallPlayerToAction()
+          ) : (
+            <div className="winner">
+              <h1>{this.renderWinner()}</h1>
+            </div>
+          )}
+          {!game.isGameOver() ? <InGameControls {...controlProps} /> : <InGameOverControls {...controlProps} />}
         </div>
       </div>
     );
