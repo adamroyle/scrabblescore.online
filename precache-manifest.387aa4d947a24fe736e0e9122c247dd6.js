56e4984ca43e9e5c8a2586aa9ea11e483dac31b0
Adam Royle <adam@ifunk.net>
2024-01-02 20:43:46 +1000

    Deploy

---
+++ precache-manifest.387aa4d947a24fe736e0e9122c247dd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "977b7abb84ae8d36cf8b839e2b555b15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "839028c5699ac0e555be",
+    "url": "/static/css/2.06b826f2.chunk.css"
+  },
+  {
+    "revision": "7ad89fc46c69b9835dac",
+    "url": "/static/css/main.e919ad65.chunk.css"
+  },
+  {
+    "revision": "839028c5699ac0e555be",
+    "url": "/static/js/2.297c13b2.chunk.js"
+  },
+  {
+    "revision": "b2f8dd8c666aee86b400f6f989b4df6a",
+    "url": "/static/js/2.297c13b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ad89fc46c69b9835dac",
+    "url": "/static/js/main.b0291625.chunk.js"
+  },
+  {
+    "revision": "d227fdb23fb756b9c188",
+    "url": "/static/js/runtime-main.620604b8.js"
+  }
+]);
